85b807e4dca51dc3a7fbc74149d642a84cc4ad2f
Duaa Alahmed <dody@Duaas-MacBook-Pro.local>
2017-11-28 22:01:46 -0700

    added the new function

--- geometry_test.c
+++ geometry_test.c
@@ -145,6 +145,36 @@ START_TEST(test_2d_midpoint)
 }
 END_TEST
 
+START_TEST(test_2d_area_triangle)
+{
+    coord_2d_t a;
+    coord_2d_t b;
+    coord_2d_t c;
+    double exp;
+
+    a.x = 5;
+    a.y = 5;
+    b.x = 10;
+    b.y = 10;
+    c.x = 20;
+    c.y = 5;
+    exp = 37.5;
+    ck_assert( coord_2d_area_triangle(&a, &b, &c) == exp);
+
+    a.x = 5;
+    a.y = 5;
+    b.x = 15;
+    b.y = 15;
+    c.x = 20;
+    c.y = 5;
+    exp = 75;
+   
+   ck_assert( coord_2d_area_triangle(&a, &b, &c) == exp);
+
+
+}
+END_TEST
+
 /* coord_2d Test Suite */
 Suite* coord_2d_suite(void)
 {
@@ -161,6 +191,9 @@ Suite* coord_2d_suite(void)
 
     TCase* tc_2d_midpoint = tcase_create("coord_2d_midpoint");
     tcase_add_test(tc_2d_midpoint, test_2d_midpoint);
+
+    TCase* tc_2d_area_triangle = tcase_create("coord_2d_area_triangle");
+    tcase_add_test(tc_2d_area_triangle, test_2d_area_triangle);
 
     /* Add Cases to Suite */
     suite_add_tcase(s, tc_2d_eq);
